a6f8f25b20b4bc0242022d6b0cd1f2e44e5d76aa
Said <saidbachar.uca.ma@gmail.com>
2020-11-29 02:04:28 +0100

    Detect Active Menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,7 +81,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </router-link>
               </li>
           <li class="nav-item has-treeview">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-cog"></i>
               <p>
                   Management
@@ -90,7 +90,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="fa fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
